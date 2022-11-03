a86d13b949acf6fe8c601f7a99de3ca6d734e72c
ajohnston <AlbertJohnstonCS@gmail.com>
2022-11-03 11:30:05 -0700

    Add block values to link siblings in TokenizerState

--- src/main/kotlin/com/techempower/openhttpheaders/parse/TokenizerState.kt
+++ src/main/kotlin/com/techempower/openhttpheaders/parse/TokenizerState.kt
@@ -4,7 +4,8 @@ package com.techempower.openhttpheaders.parse
 internal value class InlineTokenizerSavePoint(val value: Long)
 
 internal interface TokenizerState {
-  // Returns the index of the first value added
+  // Returns the index of the block added. Can be used to modify/retrieve
+  // values for/from all other functions.
   fun add(
       // Unique per grammar instance. Incremented from 0 using a static
       // AtomicInteger in the Grammar class.
@@ -16,8 +17,9 @@ internal interface TokenizerState {
       rangeStartInclusive: Int,
       rangeEndExclusive: Int,
       // Always used to indicate the number of children following the grammar.
-      // May not end up being necessary but support for now.
       numberOfChildren: Int,
+      // The index of the previous sibling. Not guaranteed to be the previous block over.
+      previousSiblingIndex: Int,
   ): Int
 
   fun update(
@@ -44,33 +46,47 @@ internal interface TokenizerState {
 
   fun getNumberOfChildren(index: Int): Int
 
+  fun getPreviousSiblingIndex(index: Int): Int
+
+  fun getNextSiblingIndex(index: Int): Int
+
+  fun setNextSiblingIndex(index: Int, nextSiblingIndex: Int)
+
+  fun getNthChildIndex(parentIndex: Int, childIndexFromParent: Int): Int
+
   fun save(): InlineTokenizerSavePoint
 
   fun restore(savePoint: InlineTokenizerSavePoint)
+
+  companion object {
+    const val NOT_FOUND = -1
+    const val SKIP = -2
+  }
 }
 
 internal class TokenizerStateImpl : TokenizerState {
 
+  // Current tokenizer index
+  private var inputIndex = 0
   private val ints = IntList()
-
-  init {
-    // Current tokenizer index
-    ints += 0
-  }
 
   override fun add(
       grammarId: Int,
       grammarValue: Int,
       rangeStartInclusive: Int,
       rangeEndExclusive: Int,
-      numberOfChildren: Int
+      numberOfChildren: Int,
+      previousSiblingIndex: Int
   ): Int {
+    val index = ints.size / BLOCK_SIZE
     ints += grammarId
     ints += grammarValue
     ints += rangeStartInclusive
     ints += rangeEndExclusive
     ints += numberOfChildren
-    return  (ints.size - 1) / BLOCK_SIZE
+    ints += previousSiblingIndex
+    ints += TokenizerState.NOT_FOUND
+    return index
   }
 
   override fun update(
@@ -78,7 +94,7 @@ internal class TokenizerStateImpl : TokenizerState {
       grammarValue: Int,
       rangeStartInclusive: Int,
       rangeEndExclusive: Int,
-      numberOfChildren: Int
+      numberOfChildren: Int,
   ) {
     val offsetIndex = offsetIndex(index)
     ints[offsetIndex + 1] = grammarValue
@@ -88,13 +104,13 @@ internal class TokenizerStateImpl : TokenizerState {
   }
 
   override fun setInputIndex(index: Int) {
-    ints[0] = index
+    inputIndex = index
   }
 
-  override fun getInputIndex(): Int = ints[0]
+  override fun getInputIndex(): Int = inputIndex
 
   override fun isIndexPopulated(index: Int): Boolean {
-    return ints.size > BLOCK_SIZE * index + 1
+    return index >= 0 && ints.size > offsetIndex(index)
   }
 
   override fun getGrammarId(index: Int): Int = ints[offsetIndex(index)]
@@ -110,7 +126,33 @@ internal class TokenizerStateImpl : TokenizerState {
   override fun getNumberOfChildren(index: Int): Int =
       ints[offsetIndex(index) + 4]
 
-  private fun offsetIndex(index: Int) = BLOCK_SIZE * index + 1
+  override fun getPreviousSiblingIndex(index: Int): Int =
+      ints[offsetIndex(index) + 5]
+
+  override fun getNextSiblingIndex(index: Int): Int =
+      ints[offsetIndex(index) + 6]
+
+  override fun setNextSiblingIndex(index: Int, nextSiblingIndex: Int) {
+    ints[offsetIndex(index) + 6] = nextSiblingIndex
+  }
+
+  override fun getNthChildIndex(parentIndex: Int, childIndexFromParent: Int): Int {
+    val numberOfChildren = getNumberOfChildren(parentIndex)
+    if (numberOfChildren <= childIndexFromParent) {
+      throw IndexOutOfBoundsException("Child index ($childIndexFromParent) " +
+          "is not within the bounds of the parent index's number of " +
+          "children ($numberOfChildren)")
+    }
+    // This for loop does not require allocation:
+    // https://pancake.coffee/2021/08/12/kotlin-for-loop-with-no-additional-object-allocation/
+    var nextIndex = parentIndex + 1
+    for(i in 0 until childIndexFromParent) {
+      nextIndex = getNextSiblingIndex(nextIndex)
+    }
+    return nextIndex
+  }
+
+  private fun offsetIndex(index: Int) = BLOCK_SIZE * index
 
   override fun save(): InlineTokenizerSavePoint {
     // Two ints to a long based on https://stackoverflow.com/a/12772968
@@ -127,11 +169,18 @@ internal class TokenizerStateImpl : TokenizerState {
     val inputIndex = (value shr 32).toInt()
     val intSize = value.toInt()
     setInputIndex(inputIndex)
+    val blockIndex = intSize / BLOCK_SIZE
+    if (isIndexPopulated(blockIndex)) {
+      val previousSiblingIndex = getPreviousSiblingIndex(blockIndex)
+      if (isIndexPopulated(blockIndex)) {
+        setNextSiblingIndex(previousSiblingIndex, -1)
+      }
+    }
     ints.truncate(intSize)
   }
 
   companion object {
     // Must be equal to the number of properties set in `add(...)`
-    private const val BLOCK_SIZE = 5
+    private const val BLOCK_SIZE = 7
   }
 }
--- src/test/kotlin/com/techempower/openhttpheaders/parse/TokenizerStateImplTest.kt
+++ src/test/kotlin/com/techempower/openhttpheaders/parse/TokenizerStateImplTest.kt
@@ -13,6 +13,41 @@ class TokenizerStateImplTest : FunSpec({
         rangeStartInclusive = 8,
         rangeEndExclusive = 11,
         numberOfChildren = 15,
+        previousSiblingIndex = 17,
+    ) shouldBe 0
+    state.add(
+        grammarId = 22,
+        grammarValue = 55,
+        rangeStartInclusive = 88,
+        rangeEndExclusive = 111,
+        numberOfChildren = 155,
+        previousSiblingIndex = 177,
+    ) shouldBe 1
+    state.getGrammarId(0) shouldBe 2
+    state.getGrammarValue(0) shouldBe 5
+    state.getRangeStartInclusive(0) shouldBe 8
+    state.getRangeEndExclusive(0) shouldBe 11
+    state.getNumberOfChildren(0) shouldBe 15
+    state.getPreviousSiblingIndex(0) shouldBe 17
+    state.getNextSiblingIndex(0) shouldBe TokenizerState.NOT_FOUND
+
+    state.getGrammarId(1) shouldBe 22
+    state.getGrammarValue(1) shouldBe 55
+    state.getRangeStartInclusive(1) shouldBe 88
+    state.getRangeEndExclusive(1) shouldBe 111
+    state.getNumberOfChildren(1) shouldBe 155
+    state.getPreviousSiblingIndex(1) shouldBe 177
+    state.getNextSiblingIndex(1) shouldBe TokenizerState.NOT_FOUND
+  }
+  test("set/getNextSiblingIndex") {
+    val state = TokenizerStateImpl()
+    state.add(
+        grammarId = 2,
+        grammarValue = 5,
+        rangeStartInclusive = 8,
+        rangeEndExclusive = 11,
+        numberOfChildren = 15,
+        previousSiblingIndex = 17,
     )
     state.add(
         grammarId = 22,
@@ -20,18 +55,23 @@ class TokenizerStateImplTest : FunSpec({
         rangeStartInclusive = 88,
         rangeEndExclusive = 111,
         numberOfChildren = 155,
+        previousSiblingIndex = 177,
     )
-    state.getGrammarId(0) shouldBe 2
-    state.getGrammarValue(0) shouldBe 5
-    state.getRangeStartInclusive(0) shouldBe 8
-    state.getRangeEndExclusive(0) shouldBe 11
-    state.getNumberOfChildren(0) shouldBe 15
+    // Sanity check
+    state.getNextSiblingIndex(0) shouldBe TokenizerState.NOT_FOUND
+    state.getNextSiblingIndex(1) shouldBe TokenizerState.NOT_FOUND
 
-    state.getGrammarId(1) shouldBe 22
-    state.getGrammarValue(1) shouldBe 55
-    state.getRangeStartInclusive(1) shouldBe 88
-    state.getRangeEndExclusive(1) shouldBe 111
-    state.getNumberOfChildren(1) shouldBe 155
+    state.setNextSiblingIndex(0, 0)
+
+    state.getNextSiblingIndex(0) shouldBe 0
+    state.getNextSiblingIndex(1) shouldBe TokenizerState.NOT_FOUND
+
+    shouldThrowUnit<IndexOutOfBoundsException> {
+      state.setNextSiblingIndex(2, 0)
+    }
+    shouldThrowUnit<IndexOutOfBoundsException> {
+      state.getNextSiblingIndex(2)
+    }
   }
   test("set/getCurrentIndex") {
     val state = TokenizerStateImpl()
@@ -41,10 +81,34 @@ class TokenizerStateImplTest : FunSpec({
   }
   test("save/restore") {
     val state = TokenizerStateImpl()
-    state.add(1, 2, 3, 4, 5)
+    state.add(
+        grammarId = 1,
+        grammarValue = 2,
+        rangeStartInclusive = 3,
+        rangeEndExclusive = 4,
+        numberOfChildren = 5,
+        previousSiblingIndex = TokenizerState.NOT_FOUND
+    )
+    state.add(
+        grammarId = 5,
+        grammarValue = 8,
+        rangeStartInclusive = 11,
+        rangeEndExclusive = 14,
+        numberOfChildren = 15,
+        previousSiblingIndex = 0
+    )
+    state.setNextSiblingIndex(0, 1)
     state.setInputIndex(6)
     val savePoint = state.save()
-    state.add(6, 7, 8, 9, 0)
+    state.add(
+        grammarId = 6,
+        grammarValue = 7,
+        rangeStartInclusive = 8,
+        rangeEndExclusive = 9,
+        numberOfChildren = 0,
+        previousSiblingIndex = 1
+    )
+    state.setNextSiblingIndex(1, 2)
     state.setInputIndex(8)
 
     state.getInputIndex() shouldBe 8
@@ -54,12 +118,24 @@ class TokenizerStateImplTest : FunSpec({
     state.getRangeStartInclusive(0) shouldBe 3
     state.getRangeEndExclusive(0) shouldBe 4
     state.getNumberOfChildren(0) shouldBe 5
+    state.getPreviousSiblingIndex(0) shouldBe TokenizerState.NOT_FOUND
+    state.getNextSiblingIndex(0) shouldBe 1
 
-    state.getGrammarId(1) shouldBe 6
-    state.getGrammarValue(1) shouldBe 7
-    state.getRangeStartInclusive(1) shouldBe 8
-    state.getRangeEndExclusive(1) shouldBe 9
-    state.getNumberOfChildren(1) shouldBe 0
+    state.getGrammarId(1) shouldBe 5
+    state.getGrammarValue(1) shouldBe 8
+    state.getRangeStartInclusive(1) shouldBe 11
+    state.getRangeEndExclusive(1) shouldBe 14
+    state.getNumberOfChildren(1) shouldBe 15
+    state.getPreviousSiblingIndex(1) shouldBe 0
+    state.getNextSiblingIndex(1) shouldBe 2
+
+    state.getGrammarId(2) shouldBe 6
+    state.getGrammarValue(2) shouldBe 7
+    state.getRangeStartInclusive(2) shouldBe 8
+    state.getRangeEndExclusive(2) shouldBe 9
+    state.getNumberOfChildren(2) shouldBe 0
+    state.getPreviousSiblingIndex(2) shouldBe 1
+    state.getNextSiblingIndex(2) shouldBe TokenizerState.NOT_FOUND
 
     state.restore(savePoint)
 
@@ -70,31 +146,41 @@ class TokenizerStateImplTest : FunSpec({
     state.getRangeStartInclusive(0) shouldBe 3
     state.getRangeEndExclusive(0) shouldBe 4
     state.getNumberOfChildren(0) shouldBe 5
+    state.getPreviousSiblingIndex(0) shouldBe TokenizerState.NOT_FOUND
+    state.getNextSiblingIndex(0) shouldBe 1
+
+    state.getGrammarId(1) shouldBe 5
+    state.getGrammarValue(1) shouldBe 8
+    state.getRangeStartInclusive(1) shouldBe 11
+    state.getRangeEndExclusive(1) shouldBe 14
+    state.getNumberOfChildren(1) shouldBe 15
+    state.getPreviousSiblingIndex(1) shouldBe 0
+    state.getNextSiblingIndex(1) shouldBe TokenizerState.NOT_FOUND
 
     shouldThrowUnit<IndexOutOfBoundsException> {
-      state.getGrammarId(1)
+      state.getGrammarId(2)
     }
     shouldThrowUnit<IndexOutOfBoundsException> {
-      state.getGrammarValue(1)
+      state.getGrammarValue(2)
     }
     shouldThrowUnit<IndexOutOfBoundsException> {
-      state.getRangeStartInclusive(1)
+      state.getRangeStartInclusive(2)
     }
     shouldThrowUnit<IndexOutOfBoundsException> {
-      state.getRangeEndExclusive(1)
+      state.getRangeEndExclusive(2)
     }
     shouldThrowUnit<IndexOutOfBoundsException> {
-      state.getNumberOfChildren(1)
+      state.getNumberOfChildren(2)
     }
   }
   test("isIndexPopulated") {
     val state = TokenizerStateImpl()
     state.isIndexPopulated(0) shouldBe false
     state.isIndexPopulated(1) shouldBe false
-    state.add(1, 2, 3, 4, 5)
+    state.add(1, 2, 3, 4, 5, TokenizerState.NOT_FOUND)
     state.isIndexPopulated(0) shouldBe true
     state.isIndexPopulated(1) shouldBe false
-    state.add(1, 2, 3, 4, 5)
+    state.add(1, 2, 3, 4, 5, 0)
     state.isIndexPopulated(0) shouldBe true
     state.isIndexPopulated(1) shouldBe true
     state.isIndexPopulated(2) shouldBe false
@@ -107,6 +193,7 @@ class TokenizerStateImplTest : FunSpec({
         rangeStartInclusive = 8,
         rangeEndExclusive = 11,
         numberOfChildren = 15,
+        previousSiblingIndex = TokenizerState.NOT_FOUND
     )
     state.add(
         grammarId = 22,
@@ -114,13 +201,18 @@ class TokenizerStateImplTest : FunSpec({
         rangeStartInclusive = 88,
         rangeEndExclusive = 111,
         numberOfChildren = 155,
+        previousSiblingIndex = 0
     )
+    state.setNextSiblingIndex(0, 0)
+
     // Sanity check
     state.getGrammarId(0) shouldBe 2
     state.getGrammarValue(0) shouldBe 5
     state.getRangeStartInclusive(0) shouldBe 8
     state.getRangeEndExclusive(0) shouldBe 11
     state.getNumberOfChildren(0) shouldBe 15
+    state.getPreviousSiblingIndex(0) shouldBe TokenizerState.NOT_FOUND
+    state.getNextSiblingIndex(0) shouldBe 0
 
     // Sanity check
     state.getGrammarId(1) shouldBe 22
@@ -128,6 +220,8 @@ class TokenizerStateImplTest : FunSpec({
     state.getRangeStartInclusive(1) shouldBe 88
     state.getRangeEndExclusive(1) shouldBe 111
     state.getNumberOfChildren(1) shouldBe 155
+    state.getPreviousSiblingIndex(1) shouldBe 0
+    state.getNextSiblingIndex(1) shouldBe TokenizerState.NOT_FOUND
 
     state.update(
         index = 0,
@@ -142,6 +236,8 @@ class TokenizerStateImplTest : FunSpec({
     state.getRangeStartInclusive(0) shouldBe 5
     state.getRangeEndExclusive(0) shouldBe 18
     state.getNumberOfChildren(0) shouldBe 19
+    state.getPreviousSiblingIndex(0) shouldBe TokenizerState.NOT_FOUND
+    state.getNextSiblingIndex(0) shouldBe 0
 
     state.update(
         index = 1,
@@ -156,6 +252,8 @@ class TokenizerStateImplTest : FunSpec({
     state.getRangeStartInclusive(1) shouldBe 55
     state.getRangeEndExclusive(1) shouldBe 188
     state.getNumberOfChildren(1) shouldBe 199
+    state.getPreviousSiblingIndex(1) shouldBe 0
+    state.getNextSiblingIndex(1) shouldBe TokenizerState.NOT_FOUND
 
     shouldThrowUnit<IndexOutOfBoundsException> {
       state.update(
@@ -165,6 +263,91 @@ class TokenizerStateImplTest : FunSpec({
           rangeEndExclusive = 0,
           numberOfChildren = 0
       )
+    }
+  }
+  test("getNthChildIndex") {
+    val state = TokenizerStateImpl()
+    // Note that this functionality is also dependent on the grammars using it
+    // correctly. If a block is added in an unexpected order, this will break.
+    // With that in mind, this test will reflect the intended usage.
+
+    // Parent grammar adds itself before checking to see if any children match
+    val parentIndex = state.add(
+        grammarId = 2,
+        grammarValue = 0,
+        rangeStartInclusive = 0,
+        rangeEndExclusive = 0,
+        numberOfChildren = 0,
+        previousSiblingIndex = TokenizerState.NOT_FOUND
+    )
+    // Child #1 matches
+    val child1Index = state.add(
+        grammarId = 7,
+        grammarValue = 0,
+        rangeStartInclusive = 0,
+        rangeEndExclusive = 0,
+        numberOfChildren = 0,
+        previousSiblingIndex = TokenizerState.NOT_FOUND
+    )
+    // Grandchild 1, should be skipped over
+    state.add(
+        grammarId = 8,
+        grammarValue = 8,
+        rangeStartInclusive = 10,
+        rangeEndExclusive = 18,
+        numberOfChildren = 0,
+        previousSiblingIndex = TokenizerState.NOT_FOUND
+    )
+    // Child #1, having matched, updates itself
+    state.update(
+        index = child1Index,
+        grammarValue = 8,
+        rangeStartInclusive = 10,
+        rangeEndExclusive = 18,
+        numberOfChildren = 1
+    )
+    // Child #1 matches
+    val child2Index = state.add(
+        grammarId = 9,
+        grammarValue = 0,
+        rangeStartInclusive = 0,
+        rangeEndExclusive = 0,
+        numberOfChildren = 0,
+        previousSiblingIndex = child1Index
+    )
+    // Grandchild 2, should be skipped over
+    state.add(
+        grammarId = 13,
+        grammarValue = 5,
+        rangeStartInclusive = 18,
+        rangeEndExclusive = 23,
+        numberOfChildren = 0,
+        previousSiblingIndex = TokenizerState.NOT_FOUND
+    )
+    // Child #2, having matched, updates itself
+    state.update(
+        index = child2Index,
+        grammarValue = 5,
+        rangeStartInclusive = 18,
+        rangeEndExclusive = 23,
+        numberOfChildren = 1
+    )
+    // The parent now updates the first child to be aware of the second
+    state.setNextSiblingIndex(child1Index, child2Index)
+    // The parent, now done matching, updates itself
+    state.update(
+        index = parentIndex,
+        grammarValue = 2,
+        rangeStartInclusive = 10,
+        rangeEndExclusive = 23,
+        numberOfChildren = 2
+    )
+
+    state.getNthChildIndex(parentIndex, 0) shouldBe child1Index
+    state.getNthChildIndex(parentIndex, 1) shouldBe child2Index
+
+    shouldThrowUnit<IndexOutOfBoundsException> {
+      state.getNthChildIndex(parentIndex, 2)
     }
   }
 })
